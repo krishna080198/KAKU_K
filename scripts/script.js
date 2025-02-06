b9b3f10e4ea7125c7df9956c5b8e21a67dd9dc57
Your Name <your-email@example.com>
2025-02-06 10:52:31 +0530

    First commit

--- scripts/script.js
+++ scripts/script.js
@@ -225,3 +225,11 @@ function activate(e) {
   e.target.matches('.prev') && slider.append(items[items.ngth -1]);
 }
 document.addEventListener('click',activate,false);
+const io = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0]);
+  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+}
+document.addEventListener('click',activate,false);
